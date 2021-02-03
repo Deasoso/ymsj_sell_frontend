fd52c7a3c76acb58ddad7b20843580158a84247d
Deasoso <932152571@qq.com>
2021-02-03 22:03:08 +0800

    卡牌大全页面优化。

--- src/views/AllCards/AllCards.vue
+++ src/views/AllCards/AllCards.vue
@@ -2,47 +2,59 @@
   <div>
     <section class="is-cover allheight backpic">
       <div class="searchmore">
-        查看更多
-        <img class="arrowlogo" src="../../assets/allcards_slices/goto.png">
+        <nav class="level">
+          <div class="level-left">
+          </div>
+          <div class="level-right">
+            查看更多
+            <img class="arrowlogo" src="../../assets/allcards_slices/goto.png">
+          </div>
+        </nav>
       </div>
       <div class="allcards">
-        <img class="teamlogo" src="../../assets/title_slices/bgi1.png">
-        <div class="teamcards">
-          <vue-scroll :ops="ops">
-            <div :style="getCardsWidth([1,1,1,1,1,1,1])">
-              <img class="teamcard1" src="../../assets/title_slices/bgi1.png">
-              <img class="teamcard" src="../../assets/title_slices/bgi1.png">
-              <img class="teamcard" src="../../assets/title_slices/bgi1.png">
-              <img class="teamcard" src="../../assets/title_slices/bgi1.png">
-              <img class="teamcard" src="../../assets/title_slices/bgi1.png">
-              <img class="teamcard" src="../../assets/title_slices/bgi1.png">
-              <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+        <nav class="level">
+          <div class="level-left">
+            <img class="teamlogo" src="../../assets/title_slices/bgi1.png">
+          </div>
+          <div class="level-right">
+            <div class="teamcards">
+              <vue-scroll :ops="ops">
+                <div :style="getCardsWidth([1,1,1,1,1,1,1])">
+                  <img class="teamcard1" src="../../assets/title_slices/bgi1.png">
+                  <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+                  <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+                  <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+                  <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+                  <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+                  <img class="teamcard" src="../../assets/title_slices/bgi1.png">
+                </div>
+              </vue-scroll>
             </div>
-          </vue-scroll>
-        </div>
+          </div>
+        </nav>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        ops: {
-          scrollPanel: {
-            scrollingX: true,
-            scrollingY: false,
-          }
-        },
-      }
-    },
-    methods:{
-      getCardsWidth(cards){
-        return "width:" + ((cards.length * 256) + ((cards.length - 1) * 24)) + "px;";
+export default {
+  data() {
+    return {
+      ops: {
+        scrollPanel: {
+          scrollingX: true,
+          scrollingY: false,
+        }
       }
     }
+  },
+  methods:{
+    getCardsWidth(cards){
+      return "width:" + ((cards.length * 256) + ((cards.length - 1) * 24)) + "px;";
+    }
   }
+}
 </script>
 
 <style scoped>
@@ -56,10 +68,11 @@
   height: 840px;
 }
 .searchmore{
-  margin-right: 272px;
-  text-align: right;
+  max-width: 1376px;
+  margin: 0 auto;
   font-size: 16px;
   padding-top: 48px;
+  width: calc(100vw - 96px);
 }
 .arrowlogo{
   width: 9px;
@@ -69,16 +82,12 @@
 .teamlogo{
   width: 128px;
   height: 174px;
-  margin-left: 272px;
-  margin-top: 85px;
-}
-.allcards{
-  text-align: left;
+  /* margin-left: 48px; */
+  /* margin-top: 85px; */
 }
 .teamcards{
-  margin-left: 448px;
-  margin-top: -254px;
-  width: calc(100vw - 448px - 272px);
+  width: calc(100vw - 144px - 128px);
+  max-width: 1200px;
   height: 367px;
 }
 .teamcard1{
@@ -89,7 +98,13 @@
 .teamcard{
   width: 256px;
   height: 367px;
-  margin-left: 24px;
   display: inline-block;
+  margin-left: 24px;
+}
+.allcards{
+  max-width: 1376px;
+  margin: 0 auto;
+  padding-top: 24px;
+  width: calc(100vw - 96px);
 }
 </style>
